3b94065fce7a98c729685b81c71b06cf15e57556
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-01-10 14:26:14 +0100

    Fixed storage issues and added 2 timers

--- options/options.js
+++ options/options.js
@@ -17,18 +17,27 @@ breakTimeOption.addEventListener("change", (event) => {
 const workSaveBtn = document.getElementById("work-save-btn");
 workSaveBtn.addEventListener("click", () => {
   chrome.storage.local.set({
-    workTimeOption: workTimeOption.value,
+    timer: 0,
+    workTimer: workTimeOption.value,
+    isRunning: false,
   });
 });
 
 const breakSaveBtn = document.getElementById("break-save-btn");
 breakSaveBtn.addEventListener("click", () => {
   chrome.storage.local.set({
-    breakTimeOption: breakTimeOption.value,
+    timer: 0,
+    breakTimer: breakTimeOption.value,
+    isRunning: false,
   });
 });
 
-chrome.storage.local.get(["workTimer", "breakTimer"], (res) => {
+chrome.storage.local.get(["workTimer"], (res) => {
   workTimeOption.value = res.workTimer;
+  console.log("Initial workTimer: ", res.workTimer);
+});
+
+chrome.storage.local.get(["breakTimer"], (res) => {
   breakTimeOption.value = res.breakTimer;
+  console.log("Initial breakTimer: ", res.breakTimer);
 });
